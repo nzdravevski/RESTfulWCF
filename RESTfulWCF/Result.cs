030eac613be68bbec392228e156849c49f2dd64e
Nikola Zdravevski <nikolazdravevski@gmail.com>
2011-12-18 23:13:55 +0100

    registration post method implemented1

---
+++ RESTfulWCF/Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace RESTfulWCF
+{
+    [DataContract]
+    public class Result
+    {
+        [DataMember]
+        public bool result { get; set; }
+    }
+}
