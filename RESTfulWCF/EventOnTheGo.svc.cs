bfc02e01fba2ad031f45778951f55948e79167af
Nikola Zdravevski <nikolazdravevski@gmail.com>
2011-12-18 23:08:30 +0100

    registration post method implemented

--- RESTfulWCF/EventOnTheGo.svc.cs
+++ RESTfulWCF/EventOnTheGo.svc.cs
@@ -66,8 +66,40 @@ namespace RESTfulWCF
 
         public Result Registration(User user)
         {
-            Result temp = new Result();
-            temp.result = true;
+            Result temp = null;
+            
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "INSERT INTO Users (firstName, lastName, username, password, email) VALUES (@firstName, @lastName, @username, @password, @email)";
+            command.Parameters.AddWithValue("@firstName", user.firstName);
+            command.Parameters.AddWithValue("@lastName", user.lastName);
+            command.Parameters.AddWithValue("@username", user.username);
+            command.Parameters.AddWithValue("@password", user.password);
+            command.Parameters.AddWithValue("@email", user.email);
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception) 
+            {
+                temp = new Result
+                {
+                    result = false
+                };
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            temp = new Result
+            {
+                result = true
+            };
 
             return temp;
         }
